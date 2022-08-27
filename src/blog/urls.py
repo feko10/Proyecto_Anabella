4cc8346606c07582be9266c60229f8fc9b06d98d
Federico Kozak <109247718+feko10@users.noreply.github.com>
2022-08-27 12:23:15 -0300

    quilombo

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -6,5 +6,7 @@ from django.conf.urls.static import static
 #En estos dos últimos import, le decimos a DJANGO que desde la configuración de django importe las configuraciones de setting y los archivos estaticos. Especificamente en este último le decimos que busque, dentro de la configuración de urls, y dentro de ello el dir statics.
 #En el caso del import de settings, importamos todo porque allí seleccionamos creamos las variables de las carpetas medias y deberían figurar acá también."""
 urlpatterns = [
-    path('blog', views.blog, name="Blog"),
-]
+    path('', views.blog, name="Blog"),
+    path('categoria/<int:categoria_id>/', views.categoria, name="categoria"),
+]#El segundo path hace referencia a una pagina que nos reenviará desde blog, a categorias.html, pero notese que se pone <categoria_id> donde ese nombre responde a la columna de la base de dato que nos interesa para esta página y se encierra entre <>. Para eso hay que chequear como está escrito la columna. Se pone como segundo parametro el views que usará al respecto
+#VER VIEWS
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from blog.models import Post #Con esto importamos la clase Post del models.py y luego creamos una variable llamada posts igual a todos los objetos de la clase Post dentro de este views. Luego le pasamos al render como tercer argumentos pasamos un diccionario llamado posts igual a posts
+from blog.models import Post, Categoria #Con esto importamos la clase Post del models.py y luego creamos una variable llamada posts igual a todos los objetos de la clase Post dentro de este views. Luego le pasamos al render como tercer argumentos pasamos un diccionario llamado posts igual a posts
 def blog(request):
     posts=Post.objects.all()
     return render(request, "blog/blog.html", {"posts": posts}) #variable post igual a todos los objetos de la clase post, y el ctx (tercer argumento) es igual a cada elemento de post.
 # Recordar que nuestra plantilla está dentro de la misma carpeta templates/blog de esta misma app. Por eso el blog/blog.
 #AL TERMINAR ACÁ NOS VAMOS AL TEMPLATES BLOG.
+
+def categoria(request, categoria_id):
+    categoria=Categoria.objects.get(id=categoria_id)
+    posts=Post.objects.filter(categorias=categoria)
+    return render(request, "blog/categoria.html", {"categoria": categoria}, {"posts": posts})
+#Esta función se vinculará a categorias.html y además del request, usa como parametro el nombre de alguna columna de la base de datos que queremos que tome.
+#Luego creamos una variable (categoria) que tome de la clase categoria sus objetos, pero filtrados (get) por categoria id.
+#Después creamos otra variable que sea igual a todos los posts, pero filtrado por la variable categoria.
+#Luego le ponemos que nos devuelva todo eso, es decir, el tipico request, dentro de la urls que creamos al respecto, filtrado por categoría y por posts correspondientes a esas categorias  en su ctx. VER URLS
